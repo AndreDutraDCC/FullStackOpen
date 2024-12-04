790fc6cb49e29f21912d782a0735c78edb657bc5
AndreDutraDCC <cienciandre@gmail.com>
2024-12-04 15:46:17 -0300

    Exercise 2.11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({persons}) => (
   <div>
@@ -27,15 +28,18 @@ const PersonForm = ({name, handleName, number, handleNumber, handleSubmit}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameQuerry, setNameQuerry] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handlePerson = event => {
     event.preventDefault()
