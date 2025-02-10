cdae7bcd06cf1bddc8a64161d8f5d9ece64e8308
AndreDutraDCC <cienciandre@gmail.com>
2025-02-10 15:33:39 -0300

    Exercise 3.11 completed

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -11,6 +11,7 @@ morgan.token('body', (request, response) => {
   : " "
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
