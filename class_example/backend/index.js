bea033854311702c1d5d8612327fc9b6b122c22b
AndreDutraDCC <cienciandre@gmail.com>
2025-02-11 15:15:39 -0300

    class_example update

--- class_example/backend/index.js
+++ class_example/backend/index.js
@@ -1,19 +1,20 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
+const Note = require('./models/note')
 
-const requestLogger = () => (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+morgan.token('body', (request, response) => {
+  return Object.keys(request.body).length !== 0
+  ? JSON.stringify(request.body) : " "
+})
+
+const app = express()
 
 app.use(cors())  
 app.use(express.json())
 app.use(express.static('dist'))
-//app.use(requestLogger())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -45,19 +46,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note){
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
-    
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -92,20 +89,19 @@ app.post('/api/notes', (request, response) => {
     const body = request.body
 
     if(!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        console.log(savedNote)
+        response.json(savedNote)
+    })
 
-    response.json(note)
 })
 
 const unknownEndpoint = () => (request, response) => {
@@ -115,7 +111,7 @@ const unknownEndpoint = () => (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
